766913ae30086564653a298b0a69b6f95dd0411c
Jessica <aleejess@gmail.com>
2022-09-07 00:27:52 -0500

    feat: distribute auth in order to include middleware

--- routes.js
+++ routes.js
@@ -1,9 +1,12 @@
 const router = require('./api/healthcheck/index');
 const user = require('./api/users/index');
+const authLocal = require('./auth/local');
 
 function routes(app) {
   app.use('/api/healthcheck', router);
   app.use('/api/users', user);
+
+  app.use('/auth/local', authLocal);
 }
 
 module.exports = routes;
